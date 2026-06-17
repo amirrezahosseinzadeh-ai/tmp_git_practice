fca4268827ed43265c7ee7538d33d88a7858c4f3
Amirreza Hosseinzadeh <amirreza.hosseinzadeh.ai@gmail.com>
2026-06-17 12:35:18 +0330

    main.py file edited

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
 print("Hello World")
 print("New line")
 print("another new line")
-
-print("Devo bere caffe oggi")
-
-print("Its a good day fratello!")
-
-print("How to use git commit --amend?")
-
-
-print("it was hard but golden!")
-
-print("Hey hello again")
-
-
-print("Hello again and again and again signore!")
