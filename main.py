291dec31964cea4eb340dd061a8cb909cc950b86
Amirreza Hosseinzadeh <amirreza.hosseinzadeh.ai@gmail.com>
2026-06-18 11:55:18 +0330

    docstring added for main file

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ print("Hello World")
 print("New line")
 print("another new line")
 
-
+"""a function defined for ...
+    a: value A
+    b: value B
+"""
 print("I want to see handle merge conflict")
 def func(a, b):
     if a > b:
         pass
     else:
         return
+
